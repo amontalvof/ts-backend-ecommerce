c24e4c924250203b337c107c64dae479f98967e3
amontalvof <amontalvof92@gmail.com>
2022-03-01 00:33:10 -0600

    fix: improved resolve products query

--- src/helpers/resolveProductsQuery.ts
+++ src/helpers/resolveProductsQuery.ts
@@ -1,6 +1,7 @@
 interface IResolveProductsQueryParams {
     ordenar?: string;
     modo?: string;
+    busqueda?: string;
     item?: string | null;
     valor?: number | null;
     base?: number;
@@ -8,11 +9,14 @@ interface IResolveProductsQueryParams {
 }
 
 const resolveProductsQuery = (body: IResolveProductsQueryParams): string => {
-    const { ordenar, item, valor, base, tope, modo } = body;
+    const { ordenar, item, valor, base, tope, modo, busqueda } = body;
+    if (busqueda) {
+        return `SELECT productos.*, categorias.categoria, categorias.ruta as categoriaRuta, subcategorias.subcategoria, subcategorias.ruta as subcategoriaRuta FROM productos LEFT JOIN categorias on productos.id_categoria=categorias.id LEFT JOIN subcategorias ON productos.id_subcategoria = subcategorias.id WHERE productos.ruta like '%${busqueda}%' OR productos.titulo like '%${busqueda}%' OR productos.titular like '%${busqueda}%' OR productos.descripcion like '%${busqueda}%' OR categorias.categoria like '%${busqueda}%' OR subcategorias.subcategoria like '%${busqueda}%' OR categorias.ruta like '%${busqueda}%' OR subcategorias.ruta like '%${busqueda}%' ORDER BY productos.${ordenar} ${modo} LIMIT ${base}, ${tope}`;
+    }
     if (item !== null) {
-        return `SELECT productos.*, categorias.categoria, categorias.ruta as categoriaRuta, subcategorias.subcategoria, subcategorias.ruta as subcategoriaRuta FROM productos LEFT JOIN categorias on productos.id_categoria=categorias.id LEFT JOIN subcategorias ON productos.id_subcategoria = subcategorias.id WHERE productos.${item} = ${valor} ORDER BY ${ordenar} ${modo} LIMIT ${base}, ${tope}`;
+        return `SELECT productos.*, categorias.categoria, categorias.ruta as categoriaRuta, subcategorias.subcategoria, subcategorias.ruta as subcategoriaRuta FROM productos LEFT JOIN categorias on productos.id_categoria=categorias.id LEFT JOIN subcategorias ON productos.id_subcategoria = subcategorias.id WHERE productos.${item} = ${valor} ORDER BY productos.${ordenar} ${modo} LIMIT ${base}, ${tope}`;
     } else {
-        return `SELECT productos.*, categorias.categoria, categorias.ruta as categoriaRuta, subcategorias.subcategoria, subcategorias.ruta as subcategoriaRuta FROM productos LEFT JOIN categorias on productos.id_categoria=categorias.id LEFT JOIN subcategorias ON productos.id_subcategoria = subcategorias.id ORDER BY ${ordenar} ${modo} LIMIT ${base}, ${tope}`;
+        return `SELECT productos.*, categorias.categoria, categorias.ruta as categoriaRuta, subcategorias.subcategoria, subcategorias.ruta as subcategoriaRuta FROM productos LEFT JOIN categorias on productos.id_categoria=categorias.id LEFT JOIN subcategorias ON productos.id_subcategoria = subcategorias.id ORDER BY productos.${ordenar} ${modo} LIMIT ${base}, ${tope}`;
     }
 };
 
--- src/helpers/resolveTotalProductsQuery.ts
+++ src/helpers/resolveTotalProductsQuery.ts
@@ -1,16 +1,21 @@
 interface IResolveTotalProductsQueryParams {
     ordenar?: string;
     modo?: string;
+    busqueda?: string;
     item?: string | null;
     valor?: number | null;
 }
 
 const resolveTotalProductsQuery = (body: IResolveTotalProductsQueryParams) => {
-    const { ordenar, item, valor, modo } = body;
+    const { ordenar, item, valor, modo, busqueda } = body;
+
+    if (busqueda) {
+        return `SELECT COUNT(*) as total FROM productos LEFT JOIN categorias on productos.id_categoria=categorias.id LEFT JOIN subcategorias ON productos.id_subcategoria = subcategorias.id WHERE productos.ruta like '%${busqueda}%' OR productos.titulo like '%${busqueda}%' OR productos.titular like '%${busqueda}%' OR productos.descripcion like '%${busqueda}%' OR categorias.categoria like '%${busqueda}%' OR subcategorias.subcategoria like '%${busqueda}%' OR categorias.ruta like '%${busqueda}%' OR subcategorias.ruta like '%${busqueda}%' ORDER BY productos.${ordenar} ${modo};`;
+    }
     if (item !== null) {
-        return `SELECT COUNT(*) as total FROM productos WHERE ${item} = ${valor} ORDER BY ${ordenar} ${modo};`;
+        return `SELECT COUNT(*) as total FROM productos LEFT JOIN categorias on productos.id_categoria=categorias.id LEFT JOIN subcategorias ON productos.id_subcategoria = subcategorias.id WHERE productos.${item} = ${valor} ORDER BY productos.${ordenar} ${modo};`;
     } else {
-        return `SELECT COUNT(*) as total FROM productos ORDER BY ${ordenar} ${modo}`;
+        return `SELECT COUNT(*) as total FROM productos LEFT JOIN categorias on productos.id_categoria=categorias.id LEFT JOIN subcategorias ON productos.id_subcategoria = subcategorias.id ORDER BY productos.${ordenar} ${modo}`;
     }
 };
 
