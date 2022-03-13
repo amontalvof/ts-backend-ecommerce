34582149a58bc1f36257be6457326431d418d336
amontalvof <amontalvof92@gmail.com>
2022-03-13 15:09:35 -0600

    fix: improved resolve queries helpers

--- src/helpers/resolveTotalProductsQuery.ts
+++ src/helpers/resolveTotalProductsQuery.ts
@@ -8,14 +8,16 @@ interface IResolveTotalProductsQueryParams {
 
 const resolveTotalProductsQuery = (body: IResolveTotalProductsQueryParams) => {
     const { ordenar, item, valor, modo, busqueda } = body;
+    const orderByModo =
+        modo !== 'Rand()' ? `productos.${ordenar} ${modo}` : `${modo}`;
 
     if (busqueda) {
-        return `SELECT COUNT(*) as total FROM productos LEFT JOIN categorias on productos.id_categoria=categorias.id LEFT JOIN subcategorias ON productos.id_subcategoria = subcategorias.id WHERE productos.ruta like '%${busqueda}%' OR productos.titulo like '%${busqueda}%' OR productos.titular like '%${busqueda}%' OR productos.descripcion like '%${busqueda}%' OR categorias.categoria like '%${busqueda}%' OR subcategorias.subcategoria like '%${busqueda}%' OR categorias.ruta like '%${busqueda}%' OR subcategorias.ruta like '%${busqueda}%' ORDER BY productos.${ordenar} ${modo};`;
+        return `SELECT COUNT(*) as total FROM productos LEFT JOIN categorias on productos.id_categoria=categorias.id LEFT JOIN subcategorias ON productos.id_subcategoria = subcategorias.id WHERE productos.ruta like '%${busqueda}%' OR productos.titulo like '%${busqueda}%' OR productos.titular like '%${busqueda}%' OR productos.descripcion like '%${busqueda}%' OR categorias.categoria like '%${busqueda}%' OR subcategorias.subcategoria like '%${busqueda}%' OR categorias.ruta like '%${busqueda}%' OR subcategorias.ruta like '%${busqueda}%' ORDER BY ${orderByModo};`;
     }
     if (item !== null) {
-        return `SELECT COUNT(*) as total FROM productos LEFT JOIN categorias on productos.id_categoria=categorias.id LEFT JOIN subcategorias ON productos.id_subcategoria = subcategorias.id WHERE productos.${item} = ${valor} ORDER BY productos.${ordenar} ${modo};`;
+        return `SELECT COUNT(*) as total FROM productos LEFT JOIN categorias on productos.id_categoria=categorias.id LEFT JOIN subcategorias ON productos.id_subcategoria = subcategorias.id WHERE productos.${item} = ${valor} ORDER BY ${orderByModo};`;
     } else {
-        return `SELECT COUNT(*) as total FROM productos LEFT JOIN categorias on productos.id_categoria=categorias.id LEFT JOIN subcategorias ON productos.id_subcategoria = subcategorias.id ORDER BY productos.${ordenar} ${modo}`;
+        return `SELECT COUNT(*) as total FROM productos LEFT JOIN categorias on productos.id_categoria=categorias.id LEFT JOIN subcategorias ON productos.id_subcategoria = subcategorias.id ORDER BY ${orderByModo};`;
     }
 };
 
